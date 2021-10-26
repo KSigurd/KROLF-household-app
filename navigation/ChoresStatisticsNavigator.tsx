67ab8a45b4b079bc89a238a30a365dc8a41ec08e
liinnjohansson <93linjoh@skola.boras.se>
2021-10-26 14:11:49 +0200

    custom tab bar added, new screens and names

--- navigation/ChoresStatisticsNavigator.tsx
+++ navigation/ChoresStatisticsNavigator.tsx
@@ -1,83 +1,41 @@
-import { createMaterialTopTabNavigator  } from "@react-navigation/material-top-tabs";
+import {
+  createMaterialTopTabNavigator
+} from "@react-navigation/material-top-tabs";
 import * as React from "react";
+import CustomTopTabBar from "../components/CustomTopTabBar";
 import ChoresScreen from "../screens/ChoresScreen";
 import StatisticsScreen from "../screens/StatisticsScreen";
 
 export type TabParamList = {
-  Chores: undefined;
-  Statistics: undefined;
+  Home: undefined;
+  FirstStatistics: undefined;
+  LastStatistics: undefined;
 };
 
 const Tab = createMaterialTopTabNavigator<TabParamList>();
 
 const ChoresStatisticsNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route, navigation }) => {
-        return { tabBarStyle: {justifyContent: "center"}, tabBarLabel: navigation.isFocused() ? route.name : "",  }
-      }}
-    >
+    <Tab.Navigator tabBar={(props: any) => <CustomTopTabBar {...props} />}>
       <Tab.Screen
-        name="Chores"
+        name="Home"
         component={ChoresScreen}
-        options={{ title: "idag"}}
+        options={{
+          title: "idag",
+        }}
       />
       <Tab.Screen
-        name="Statistics"
+        name="FirstStatistics"
         component={StatisticsScreen}
         options={{ title: "förra veckan" }}
+      />
+      <Tab.Screen
+        name="LastStatistics"
+        component={StatisticsScreen}
+        options={{ title: "december", tabBarScrollEnabled: true }}
       />
     </Tab.Navigator>
   );
 };
 
 export default ChoresStatisticsNavigator;
-
-// import * as React from "react";
-// import {
-//   createMaterialTopTabNavigator,
-//   MaterialTopTabScreenProps,
-// } from "@react-navigation/material-top-tabs";
-// import BistroScreen from "../screens/BistroScreen";
-// import MapScreen from "../screens/MapScreen";
-// import { useSafeAreaInsets } from "react-native-safe-area-context";
-
-// export type TabParamList = {
-//   Bistro: undefined;
-//   Map: undefined;
-// };
-
-// export type TabScreenProps<Screen extends keyof TabParamList> =
-//   MaterialTopTabScreenProps<TabParamList, Screen>;
-
-// const Tab = createMaterialTopTabNavigator<TabParamList>();
-
-// export default function TabBistroMapNavigator() {
-//   const insets = useSafeAreaInsets();
-
-//   return (
-//     <Tab.Navigator
-//       style={{ paddingTop: insets.top, backgroundColor: "#723A45" }}
-//       initialRouteName="Bistro"
-//       screenOptions={{
-//         tabBarActiveTintColor: "#fff",
-//         tabBarStyle: { backgroundColor: "#723A45" },
-//       }}
-//     >
-//       <Tab.Screen
-//         name="Bistro"
-//         component={BistroScreen}
-//         options={{
-//           title: "Restauranger",
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Map"
-//         component={MapScreen}
-//         options={{
-//           title: "Karta",
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// }
