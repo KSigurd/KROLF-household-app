32c906091f8cd66dc49408babbe57eac226ba92e
liinnjohansson <93linjoh@skola.boras.se>
2021-10-27 14:17:18 +0200

    Changed function signature to FC

--- components/CustomTopTabBar.tsx
+++ components/CustomTopTabBar.tsx
@@ -1,6 +1,6 @@
 import { MaterialTopTabDescriptorMap, MaterialTopTabNavigationProp } from "@react-navigation/material-top-tabs/lib/typescript/src/types";
 import { NavigationState } from "@react-navigation/routers";
-import React from "react";
+import React, { FC } from "react";
 import { Animated, StyleSheet, TouchableOpacity, View } from "react-native";
 import { IconButton } from "react-native-paper";
 import { TabParamList } from "../navigation/ChoresStatisticsNavigator";
@@ -11,29 +11,29 @@ type Props = {
   navigation: MaterialTopTabNavigationProp<TabParamList>;
 }
 
-const CustomTopTabBar = (props: Props) => {
-  //Define the values for index and routes. To be able to shift between the right screens, in the right order.
-  const currentIndex = props.state.index;
-  const firstRoute = props.state.routes[0];
-  const lastRoute = props.state.routes[props.state.routes.length - 1];
-  const prevRoute = props.state.routes[currentIndex - 1];
-  const currentRoute = props.state.routes[currentIndex];
-  const nextRoute = props.state.routes[currentIndex + 1];
+const CustomTopTabBar : FC<Props> = ({state, descriptors, navigation}) => {
+  //Define the values for getting specific index and routes. (To be able to shift between the right screens, in the right order.)
+  const currentIndex = state.index;
+  const firstRoute = state.routes[0];
+  const lastRoute = state.routes[state.routes.length - 1];
+  const prevRoute = state.routes[currentIndex - 1];
+  const currentRoute = state.routes[currentIndex];
+  const nextRoute = state.routes[currentIndex + 1];
 
   //Enable usage of options to get/display the title for current/focused tab
-  const { options } = props.descriptors[currentRoute.key];
+  const { options } = descriptors[currentRoute.key];
 
   //OnPress functions for arrow buttons
   const onLeftPress = () => {
     prevRoute
-      ? props.navigation.navigate(prevRoute.name as keyof TabParamList)
-      : props.navigation.navigate(lastRoute.name as keyof TabParamList);
+      ? navigation.navigate(prevRoute.name as keyof TabParamList)
+      : navigation.navigate(lastRoute.name as keyof TabParamList);
   };
 
   const onRightPress = () => {
     nextRoute
-      ? props.navigation.navigate(nextRoute.name as keyof TabParamList)
-      : props.navigation.navigate(firstRoute.name as keyof TabParamList);
+      ? navigation.navigate(nextRoute.name as keyof TabParamList)
+      : navigation.navigate(firstRoute.name as keyof TabParamList);
   };
 
   return (
