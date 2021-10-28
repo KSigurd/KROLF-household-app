6793711fcf2040f46cf81e4f142821a0af28049d
Olivia Bodén <Olivia.annemaria@hotmail.com>
2021-10-28 15:54:36 +0200

    get chores from database

--- screens/ChoresScreen.tsx
+++ screens/ChoresScreen.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { FC } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 import { Button } from "react-native-paper";
 import ChoreButton from "../components/ChoreButton";
 import { chores } from "../data/mockChoresData";
@@ -10,20 +10,27 @@ import {
   StackScreenProps,
 } from "../navigation/RootNavigator";
 import { selectHouseholdById } from "../store/household/hoseholdSelector";
-import { useAppSelector } from "../store/store";
+import { useAppDispatch, useAppSelector } from "../store/store";
 import { styles } from "../styles/styles";
 import CreateChoreInfo from "../components/CreateChoreInfo";
+import { getChores } from "../data/fireStoreModule";
+import { getChoresAction } from "../store/chore/choreSlice";
+import { users } from "../data/mockUserData";
 
 const ChoresScreen = ({
   route,
   navigation,
 }: StackScreenProps<"ChoresStatisticsNavigator">) => {
-  
+  const dispatch = useAppDispatch();
   const activeHouseholdState = useAppSelector(
     (state) => state.household.activeHouseholdId
   );
-
   const household = useAppSelector(selectHouseholdById(activeHouseholdState));
+
+  dispatch(getChoresAction(activeHouseholdState));
+  const allHousoholdChores = useAppSelector((state) => state.chore.chores);
+
+  // console.log(allHousoholdChores);
 
   const [isVisible, setIsVisible] = React.useState(false);
 
@@ -32,21 +39,29 @@ const ChoresScreen = ({
       <Text>household ID who is active : {activeHouseholdState}</Text>
       <Text>Name : {household?.name}</Text>
 
-      {/* {household.chores.map((prop, key) => {
+      {allHousoholdChores.map((prop, index) => {
         return (
-          <ChoreButton  //TODO: Move this props-logic somewhere else
-            key={key}
-            buttonText={prop.title}
-            completedBy={users.filter(
-              (user) =>
-                user.choresDone.filter((chore) => chore.choreId === prop.id)
-                  .length
-            )}
-            daysSinceLast={String(7)}
-            isLate={false}
-          />
+          <TouchableHighlight style={{padding: 10}}key={index}>
+            <Text onPress={() => alert(`GÅ NU OCH ${prop.title}`)}>
+              {prop.title}
+            </Text>
+          </TouchableHighlight>
+
+          
+          // <ChoreButton  //TODO: Move this props-logic somewhere else
+          //   key={key}
+          //   buttonText={prop.title}
+          //   completedBy={users.filter(
+          //     (user) =>
+          //       user.choresDone.filter((chore) => chore.choreId === prop.id)
+          //         .length
+          //   )}
+
+          //   daysSinceLast={String(7)}
+          //   isLate={false}
+          // />
         );
-      })} */}
+      })}
 
       {/* TODO: flytta till styles.css */}
       <View style={styles.bottomButtonRow}>
