a6951dc642ef2c9e4eaac69af7e0af5d9bf7cc02
liinnjohansson <93linjoh@skola.boras.se>
2021-10-29 14:35:24 +0200

    Created layout for JoinHouseholdScreen

--- components/ThemedTextInput.tsx
+++ components/ThemedTextInput.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { TextInput as NPTextInput } from "react-native-paper";
+import { Surface, TextInput as NPTextInput } from "react-native-paper";
 import { TextInputProps } from "react-native-paper/lib/typescript/components/TextInput/TextInput";
 
 interface Props extends Omit<TextInputProps, "theme"> {
-  label: string;
+  label?: string;
   helperText?: string | false;
 }
 
@@ -12,13 +12,27 @@ interface Props extends Omit<TextInputProps, "theme"> {
 const ThemedTextInput = ({ label, helperText, ...textInputProps }: Props) => {
   return (
     <View style={styles.root}>
-      <Text style={styles.text}>{label}</Text>
-      <NPTextInput
-        mode="outlined"
-        theme={{ roundness: 10 }}
-        {...textInputProps}
-        style={[styles.input, helperText ? { borderColor: "red" } : null]}
-      />
+      {label ? <Text style={styles.text}>{label}</Text> : null}
+      <Surface style={styles.surface}>
+        <NPTextInput
+          mode="outlined"
+          //mode="flat"
+          //theme={{ roundness: 10}}
+          theme={{
+            roundness: 10,
+            colors: {
+              placeholder: "transparent",
+              text: "black",
+              primary: "black",
+            },
+          }}
+          {...textInputProps}
+          style={[styles.input, helperText ? { borderColor: "red" } : null]}
+          //underlineColor="transparent"
+          //underlineColorAndroid="transparent"
+        />
+      </Surface>
+
       {helperText && <Text style={styles.helperText}>{helperText}</Text>}
     </View>
   );
@@ -29,11 +43,13 @@ export default ThemedTextInput;
 const styles = StyleSheet.create({
   root: {
     marginHorizontal: 10,
-    marginVertical: 5,
-    justifyContent: "space-between",
+    marginVertical: 8,
   },
   input: {
-    elevation: 4,
+    borderColor: "transparent",
+    borderRadius: 10,
+    backgroundColor: "white",
+    marginTop: -6,
   },
   helperText: {
     color: "red",
@@ -41,5 +57,11 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     fontWeight: "bold",
+  },
+  surface: {
+    marginTop: 12,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 10,
+    elevation: 4,
   },
 });
--- screens/JoinHouseholdScreen.tsx
+++ screens/JoinHouseholdScreen.tsx
@@ -1,22 +1,15 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Formik } from "formik";
 import React, { FC, useState } from "react";
-import { View } from "react-native";
-import {
-  Text,
-  Button,
-  Surface,
-  Title,
-  TouchableRipple,
-} from "react-native-paper";
-import { styles } from "../styles/styles";
+import { StyleSheet, View } from "react-native";
+import { Button, Surface, Text, TouchableRipple } from "react-native-paper";
 import * as Yup from "yup";
 import ThemedTextInput from "../components/ThemedTextInput";
-import { Formik } from "formik";
-import { useAppDispatch, useAppSelector } from "../store/store";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { addHouseholdUserAction } from "../store/householdUser/householdUserSlice";
-import { RootStackParamList } from "../navigation/RootNavigator";
 import { avatars } from "../data/avatarData";
 import { Avatar } from "../interfaces/avatar";
+import { RootStackParamList } from "../navigation/RootNavigator";
+import { addHouseholdUserAction } from "../store/householdUser/householdUserSlice";
+import { useAppDispatch, useAppSelector } from "../store/store";
 
 interface ParamsToValidate {
   inviteCode: string;
@@ -94,7 +87,7 @@ const JoinHouseholdScreen: FC<Props> = ({ navigation }: Props) => {
         errors,
       }) => (
         <View style={styles.root}>
-          <Surface style={[styles.fullscreenButton, styles.buttonOutlined]}>
+          <View>
             <ThemedTextInput
               secureTextEntry={false}
               label="Inbjudningskod"
@@ -103,8 +96,6 @@ const JoinHouseholdScreen: FC<Props> = ({ navigation }: Props) => {
               onBlur={handleBlur<keyof ParamsToValidate>("inviteCode")}
               helperText={touched.inviteCode && errors.inviteCode}
             />
-          </Surface>
-          <Surface style={[styles.fullscreenButton, styles.buttonOutlined]}>
             <ThemedTextInput
               secureTextEntry={false}
               label="Ditt namn i hushållet"
@@ -113,43 +104,44 @@ const JoinHouseholdScreen: FC<Props> = ({ navigation }: Props) => {
               onBlur={handleBlur<keyof ParamsToValidate>("name")}
               helperText={touched.name && errors.name}
             />
-          </Surface>
-          <Surface style={[styles.avatarContainer]}>
-            <Title style={styles.buttonText}>Välj din avatar</Title>
-            <Surface style={[styles.buttonInnerContainer]}>
-              {avatars.map((prop, key) => {
-                return (
-                  <TouchableRipple
-                    key={key}
-                    borderless={true}
-                    style={[
-                      styles.repeatabilityCircle,
-                      styles.avatarButton,
-                      prop.id === avatar
-                        ? styles.selectedAvatarBackground
-                        : styles.unselectedAvatarBackground,
-                    ]}
-                    onPress={() => {values.avatarId = prop.id; chooseAvatar(prop)}}
-                  >
-                    <Text style={styles.buttonText}>{prop.avatar}</Text>
-                  </TouchableRipple>
-                );
-              })}
-            </Surface>
-          </Surface>
-          <View style={styles.bottomButtonRow}>
-            <Button
-            disabled={!values.inviteCode || !values.name || !values.avatarId}
-              icon="plus-circle-outline"
-              labelStyle={styles.buttonIconSize}
-              color={"#000"}
-              uppercase={false}
-              style={styles.smallButton}
-              onPress={() => handleSubmit()}
-            >
-              <Text style={[styles.buttonText]}>Gå med</Text>
-            </Button>
+            <View style={styles.selectAvatar}>
+              <Text style={styles.buttonText}>Välj din avatar</Text>
+              <Surface style={styles.avatarContainer}>
+                {avatars.map((prop, key) => {
+                  return (
+                    <TouchableRipple
+                      key={key}
+                      borderless={true}
+                      style={[
+                        styles.repeatabilityCircle,
+                        styles.avatarButton,
+                        prop.id === avatar
+                          ? styles.selectedAvatarBackground
+                          : styles.unselectedAvatarBackground,
+                      ]}
+                      onPress={() => {
+                        values.avatarId = prop.id;
+                        chooseAvatar(prop);
+                      }}
+                    >
+                      <Text style={styles.buttonText}>{prop.avatar}</Text>
+                    </TouchableRipple>
+                  );
+                })}
+              </Surface>
+            </View>
           </View>
+          <Button
+            icon="plus-circle-outline"
+            mode="contained"
+            color="#fff"
+            labelStyle={styles.buttonIconSize}
+            uppercase={false}
+            style={styles.NPbutton}
+            onPress={() => handleSubmit()}
+          >
+            <Text style={styles.buttonText}>Gå med</Text>
+          </Button>
         </View>
       )}
     </Formik>
@@ -157,3 +149,70 @@ const JoinHouseholdScreen: FC<Props> = ({ navigation }: Props) => {
 };
 
 export default JoinHouseholdScreen;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  selectAvatar: {
+    alignItems: "flex-start",
+    flexDirection: "column",
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  avatarContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 30,
+    borderRadius: 10,
+    backgroundColor: "white",
+    elevation: 4,
+    borderColor: "transparent",
+    width: "100%",
+    marginTop: 12,
+  },
+  repeatabilityCircle: {
+    width: 30,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    elevation: 4,
+  },
+  selectedAvatarBackground: {
+    backgroundColor: "#EDEDED",
+  },
+  unselectedAvatarBackground: {
+    backgroundColor: "white",
+  },
+  bottomButtonRow: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+  },
+  buttonIconSize: {
+    fontSize: 25,
+  },
+  NPbutton: {
+    width: 150,
+    borderRadius: 100,
+    padding: 10,
+    alignSelf: "center",
+    marginVertical: 10,
+  },
+  avatarButton: {
+    margin: 0,
+    elevation: 0,
+  },
+  input: {
+    elevation: 4,
+  },
+});
