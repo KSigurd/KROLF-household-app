326bbb3a4dbc4ca17317250d95c1440b9e9b7dd5
KSigurd <ksigurd10@gmail.com>
2021-11-01 10:51:33 +0100

    origin/feature/80-add-info-to-description-modal

--- screens/ChoreDescriptionModalScreen.tsx
+++ screens/ChoreDescriptionModalScreen.tsx
@@ -3,24 +3,29 @@ import React from "react";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 import ChoreDescription from "../components/ChoreDescription";
-import CreateChoreInfo from "../components/CreateChoreInfo";
+import { Chore } from "../interfaces/chore";
 import { TabParamList } from "../navigation/ChoresStatisticsNavigator";
 
 type Props = NativeStackScreenProps<TabParamList>;
 
-const CreateChoreModalScreen = ({ navigation }: Props) => {
+const CreateChoreModalScreen = ({ navigation, route }: Props) => {
   const onClosed = () => {
     navigation.navigate("Home");
   };
 
+  const chore = route.params;
+
+  console.log(chore);
   return (
     <View style={styles.outerContainer}>
       <SafeAreaView>
         <View style={styles.topCointainer}>
-          <Text style={styles.titleText}>Koppla sysslas namn?</Text>
+          {/* TODO: MUPPAR UR. SÄTT EN GUBBE PÅ DET */}
+          <Text style={styles.titleText}>{chore.title}</Text>
         </View>
         <View style={styles.centerContainer}>
-          <ChoreDescription onClosed={onClosed} />
+          {/* TODO: MUPPAR UR. SÄTT EN GUBBE PÅ DET */}
+          <ChoreDescription onClosed={onClosed} chore={chore} />
         </View>
       </SafeAreaView>
     </View>
--- screens/ChoresScreen.tsx
+++ screens/ChoresScreen.tsx
@@ -1,115 +1,117 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, Surface, TouchableRipple } from "react-native-paper";
+import { number } from "yup/lib/locale";
 import ChoreDescription from "../components/ChoreDescription";
+import { Chore } from "../interfaces/chore";
 import { StackScreenProps } from "../navigation/RootNavigator";
 import { getChoresAction } from "../store/chore/choreSlice";
 import { selectHouseholdById } from "../store/household/hoseholdSelector";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { styles } from "../styles/styles";
+import ChoreDescriptionModalScreen from "./ChoreDescriptionModalScreen";
 
 const ChoresScreen = ({
-    navigation,
+  navigation,
 }: StackScreenProps<"ChoresStatisticsNavigator">) => {
-    const dispatch = useAppDispatch();
-    const activeHouseholdState = useAppSelector(
-        (state) => state.household.activeHouseholdId
-    );
-    const household = useAppSelector(selectHouseholdById(activeHouseholdState));
+  const dispatch = useAppDispatch();
+  const activeHouseholdState = useAppSelector(
+    (state) => state.household.activeHouseholdId
+  );
+  const household = useAppSelector(selectHouseholdById(activeHouseholdState));
 
-    dispatch(getChoresAction(activeHouseholdState));
-    const allHousoholdChores = useAppSelector((state) => state.chore.chores);
+  dispatch(getChoresAction(activeHouseholdState));
+  const allHousoholdChores = useAppSelector((state) => state.chore.chores);
 
-    // const [isVisible, setIsVisible] = React.useState(false);
+  // const [isVisible, setIsVisible] = React.useState(false);
+  return (
+    <View style={{ flex: 1, marginHorizontal: 10, marginVertical: 25 }}>
+      <Text>household ID who is active : {activeHouseholdState}</Text>
+      <Text>Name : {household?.name}</Text>
 
-    return (
-        <View style={{ flex: 1, marginHorizontal: 10, marginVertical: 25 }}>
-            <Text>household ID who is active : {activeHouseholdState}</Text>
-            <Text>Name : {household?.name}</Text>
+      {allHousoholdChores.map((prop) => {
+        return (
+          <View key={prop.id}>
+            <Surface style={stylesLocal.surface}>
+              {/* TODO: NAVIGATE TO NEW MODAL, FILMIL */}
+              <TouchableRipple
+                style={stylesLocal.chip}
+                onPress={() => {
+                  navigation.navigate("ChoreDescriptionModalScreen", prop);
+                }}
 
-            {allHousoholdChores.map((prop) => {
-                return (
-                    <View key={prop.id}>
-                        <Surface style={stylesLocal.surface}>
-                            {/* TODO: NAVIGATE TO NEW MODAL, FILMIL */}
-                            <TouchableRipple
-                                
-                                style={stylesLocal.chip}
-                                onPress={() => navigation.navigate("ChoreDescriptionModalScreen")}
-                            >
-                                <Text
-                                    style={stylesLocal.surfaceText}
-                                >
-                                    {prop.title}
-                                </Text>
-                            </TouchableRipple>
-                        </Surface>
-                    </View>
-                );
-            })}
+                // onPress={() => {
+                //   console.log(prop.title);
+                //   navigation.navigate("ChoreDescriptionModalScreen", {id: string = prop.title?});
+                // }}
+              >
+                <Text style={stylesLocal.surfaceText}>{prop.title}</Text>
+              </TouchableRipple>
+            </Surface>
+          </View>
+        );
+      })}
 
-            {/* TODO: flytta till styles.css */}
-            <View style={styles.bottomButtonRow}>
-                <Button
-                    icon="plus-circle-outline"
-                    labelStyle={styles.buttonIconSize}
-                    color={"#000"}
-                    uppercase={false}
-                    style={styles.smallButton}
-                    onPress={() =>
-                        navigation.navigate("CreateChoreModalScreen")
-                    } //TODO
-                >
-                    <Text style={styles.buttonText}>Lägg till</Text>
-                </Button>
-                <Button
-                    icon="pencil-outline"
-                    labelStyle={styles.buttonIconSize}
-                    color={"#000"}
-                    uppercase={false}
-                    style={styles.smallButton}
-                    onPress={() => {}} //TODO
-                >
-                    <Text style={styles.buttonText}>Ändra</Text>
-                </Button>
-            </View>
-        </View>
-    );
+      {/* TODO: flytta till styles.css */}
+      <View style={styles.bottomButtonRow}>
+        <Button
+          icon="plus-circle-outline"
+          labelStyle={styles.buttonIconSize}
+          color={"#000"}
+          uppercase={false}
+          style={styles.smallButton}
+          onPress={() => navigation.navigate("CreateChoreModalScreen")} //TODO
+        >
+          <Text style={styles.buttonText}>Lägg till</Text>
+        </Button>
+        <Button
+          icon="pencil-outline"
+          labelStyle={styles.buttonIconSize}
+          color={"#000"}
+          uppercase={false}
+          style={styles.smallButton}
+          onPress={() => {}} //TODO
+        >
+          <Text style={styles.buttonText}>Ändra</Text>
+        </Button>
+      </View>
+    </View>
+  );
 };
 
 export default ChoresScreen;
 
 const stylesLocal = StyleSheet.create({
-    surface: {
-        height: "auto",
-        borderRadius: 10,
-        width: "100%",
-        alignItems: "flex-start",
-        justifyContent: "center",
-        backgroundColor: "white",
-        elevation: 4,
-        marginVertical: 5,
-    },
-    surfaceText: {
-        flex: 1,
-        width: "100%",
-        height: "100%",
-        fontSize: 18,
-        fontWeight: "bold",
-        alignItems: "flex-start",
-        alignSelf: "flex-start",
-    },
-    chip: {
-        width: "100%",
-        padding: 10,
-        backgroundColor: "white",
-        height: 50,
-        borderRadius: 10,
-    },
+  surface: {
+    height: "auto",
+    borderRadius: 10,
+    width: "100%",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    backgroundColor: "white",
+    elevation: 4,
+    marginVertical: 5,
+  },
+  surfaceText: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    fontSize: 18,
+    fontWeight: "bold",
+    alignItems: "flex-start",
+    alignSelf: "flex-start",
+  },
+  chip: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "white",
+    height: 50,
+    borderRadius: 10,
+  },
 });
 
 {
-    /* {household.chores.map((prop, key) => {
+  /* {household.chores.map((prop, key) => {
         return (
           <ChoreButton  //TODO: Move this props-logic somewhere else
             key={key}
@@ -127,5 +129,5 @@ const stylesLocal = StyleSheet.create({
 }
 
 {
-    /* <CreateChore setVisibleModal= {(isVisible)} /> */
+  /* <CreateChore setVisibleModal= {(isVisible)} /> */
 }
